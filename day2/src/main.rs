21bcd00cab880f2ce577d0d427192aa2ce7357f8
Jeff Registre <jeffreyregistre@gmail.com>
2021-12-15 23:48:50 -0500

    position

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,98 +3,101 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::ops::{AddAssign, MulAssign};
 
-#[derive(Copy, Clone, Debug)]
-struct Vector {
-    x: i32,
-    y: i32
+#[derive(Debug)]
+struct Position {
+    horizontal: i32,
+    depth: i32
 }
 
-impl Vector {
-    fn origin() -> Vector {
-        Vector {x: 0, y: 0}
+impl Position {
+    fn new(horizontal: i32, depth: i32) -> Position {
+        Position { horizontal, depth }
+    }
+    fn origin() -> Position {
+        Position { horizontal: 0, depth: 0}
     }
 }
 
-impl AddAssign for Vector {
+impl AddAssign for Position {
     fn add_assign(&mut self, rhs: Self) {
-        self.x += rhs.x;
-        self.y += rhs.y;
+        self.horizontal += rhs.horizontal;
+        self.depth += rhs.depth;
     }
 }
 
-impl MulAssign<i32> for Vector {
+impl MulAssign<i32> for Position {
     fn mul_assign(&mut self, rhs: i32) {
-        self.x *= rhs;
-        self.y *= rhs;
+        self.horizontal *= rhs;
+        self.depth *= rhs;
     }
-}
-
-fn instruct_to_vector(inst: &str) -> Vector {
-    let (command, amount) = instruct_line_to_tuple(inst);
-    let commd: &str = command.as_ref();
-
-    let mut vector = match commd {
-        "forward" => Vector {x: 1, y: 0},
-        "down" => Vector {x: 0, y: 1},
-        "up" => Vector {x: 0, y: -1},
-        _ => unreachable!("This shouldn't happen")
-    };
-
-    vector *= amount;
-    vector
-}
-
-fn instruct_line_to_tuple(inst: &str) -> (String, i32) {
-    let mut split = inst.split_whitespace().take(2);
-    let (command, amount) = (split.next().unwrap(), split.next().unwrap());
-    let amount = amount.parse().unwrap();
-    (command.to_owned(), amount)
 }
 
 fn main() {
     let filename = "input.txt";
-    let instructions = get_file(filename).expect("Expected file is loadable");
-    print_multiplied_pos(&instructions);
+    let instructions = get_parsed_instructions(filename).expect("Expected file is loadable");
+    let pos = multiplied_pos_depth(&instructions);
+    println!("The multiplied output is: {}", pos);
 
     println!("Part 2!!!");
-    with_aim(&instructions);
+    let aimed_pos = with_aim(&instructions);
+    println!("Aiming instead, our answer is: {}", aimed_pos);
 }
 
-fn with_aim(instructions: &[String]) {
-    let mut position = Vector::origin();
+fn instruction_to_offset_vector(command: &str, amount: i32) -> Position {
+    let mut unit_vector = match command {
+        "forward" => Position { horizontal: 1, depth: 0 },
+        "down" => Position { horizontal: 0, depth: 1 },
+        "up" => Position { horizontal: 0, depth: -1 },
+        _ => unreachable!(format!("Given an unexpected command: {}", command))
+    };
+
+    unit_vector *= amount;
+    unit_vector
+}
+
+fn multiplied_pos_depth(instructions: &[(String, i32)]) -> i32 {
+    let mut position = Position::origin();
+    for (command, amount) in instructions {
+        let offset = instruction_to_offset_vector(&command, *amount);
+        position += offset
+    }
+    position.horizontal * position.depth
+}
+
+
+
+fn with_aim(instructions: &[(String, i32)]) -> i32 {
+    let mut position = Position::origin();
     let mut aim = 0;
-
-    for line in instructions {
-        let (command, amount) = instruct_line_to_tuple(line);
-        let command: &str = command.as_ref();
-
-        match command  {
+    for (command, amount) in instructions {
+        match command.as_ref() {
             "down" => aim += amount,
             "up" => aim -= amount,
             "forward" => {
-                position.x += amount;
-                let tochang = amount * aim;
-                position.y += tochang;
+                position.horizontal += amount;
+                let to_change = amount * aim;
+                position.depth += to_change;
             }
-            _ => unreachable!("No")
+            _ => unreachable!(format!("Given an unexpected command: {}", command))
         }
     }
-
-    println!("The multiplied output is: {}", position.x * position.y);
+    position.horizontal * position.depth
 }
 
-fn print_multiplied_pos(instructions: &[String]) {
-    let mut position = Vector::origin();
-
-    for line in instructions {
-        let update = instruct_to_vector(&line);
-        position += update;
-    }
-    println!("The multiplied output is: {}", position.x * position.y);
-}
-
-fn get_file(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
+fn get_parsed_instructions(filename: &str) -> Result<Vec<(String, i32)>, Box<dyn Error>> {
     let f = File::open(filename)?;
-    let reader = BufReader::new(f).lines();
-    Ok(reader.flat_map(|item| item.ok()).collect())
+    let reader = BufReader::new(f);
+    let lines = reader.lines().collect::<Result<Vec<_>, _>>()?;
+
+    let mut instructions = vec![];
+    for line in lines {
+        let mut split = line.split_whitespace().take(2);
+        let (command, amount) = (
+            split.next().ok_or(format!("Can't read command for line: {}", &line))?,
+            split.next().ok_or(format!("Can't read amount for line: {}", &line))?
+        );
+        let amount: i32 = amount.parse()?;
+        instructions.push((command.to_owned(), amount))
+    }
+    Ok(instructions)
 }
